494d5f31ab3db94e534fab0f3e8a4a7805e88455
Rizal Fahrudin <rizalfahrudin879@gmail.com>
2021-09-05 20:05:30 +0700

    first commit

---
+++ iampoor/iampoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iampoor
+//
+//  Created by Rizal Fahrudin on 04/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
